a992fed88e4296bf3e35332bff218e65d3f8854b
arturfil <arturfil@hotmail.com>
2020-09-14 20:07:53 -0400

    added the helpers file | part 4 done

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require('../models/Category');
+const { errorHandler } = require('../helpers/dberrorHandler');
 
 exports.list = (req, res) => {
   Category.find().exec((err, data) => {
@@ -16,7 +17,7 @@ exports.create = (req, res) => {
   category.save((err, data) => {
     if (err) {
       return res.status(400).json({
-        message: 'Error creating the category'
+        error: errorHandler(err)
       })
     }
     res.json({data});
